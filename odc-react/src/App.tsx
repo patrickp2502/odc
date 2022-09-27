98cf1aa1ed792fddbcd25931158d236a62dab88e
patrickp2502 <patrick.pradka@gmail.com>
2022-09-27 20:09:22 +0200

    setup frontend

---
+++ odc-react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      Welcome to ODC
+    </>
+  );
+}
+
+export default App;
--- odc-react/src/Components/OperationView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OperationView = () => {
-  return (
-    <div>OperationView</div>
-  )
-}
-
-export default OperationView
